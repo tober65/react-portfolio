83b4372360029cd2579698f29b2974ff2e39c195
tober65 <timothy.ober@gmail.com>
2020-08-11 16:26:35 -0700

    Updates

---
+++ precache-manifest.5517b79731bd7773efa7e9a12609e841.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ea963f0516d957e73304ea3a515ba2",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "41b45a9b3e6bdd6c7af8",
+    "url": "/react-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6240303912ba9da13740",
+    "url": "/react-portfolio/static/js/2.0feb8754.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.0feb8754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41b45a9b3e6bdd6c7af8",
+    "url": "/react-portfolio/static/js/main.ca12264d.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
